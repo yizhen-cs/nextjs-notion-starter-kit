ba2600089b4ac085a5b54c90d4c6d3c824759312
张怡桢 <93150961+nkuzyz@users.noreply.github.com>
2024-03-25 22:21:57 +0800

    change site

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'efa29861d5004ff59504b7de6b403cf2',
+  rootNotionPageId: 'ef4dfb26413f438ca5f4210cb9d976a6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Yizhen Zhang Personal Page',
-  domain: 'https://mercurial-broccoli-1ce.notion.site/Yizhen-Zhang-efa29861d5004ff59504b7de6b403cf2',
+  domain: 'https://unmarred-almanac-c17.notion.site/ef4dfb26413f438ca5f4210cb9d976a6',
   author: 'Yizhen Zhang',
 
   // open graph metadata (optional)
