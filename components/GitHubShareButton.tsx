0bccb4455ba8fc8b5823021f088ce701909b134b
ZHANG Yizhen <93150961+yizhen-cs@users.noreply.github.com>
2025-02-19 17:55:27 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/nkuzyz'
+      href='https://github.com/yizhen-cs'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
