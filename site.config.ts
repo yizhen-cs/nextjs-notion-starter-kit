51770e4292ad5cab4d43838288f139001fa6b9f0
ZHANG Yizhen <93150961+yizhen-cs@users.noreply.github.com>
2025-02-19 17:32:14 +0800

    Update site.config.ts
    update github account

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: '',
-  github: 'nkuzyz',
+  github: 'yizhen-cs',
   linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
